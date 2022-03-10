376696cb014d17a4eca3efee6b3f1edf154d463e
Ivan Bosnić <ivan.bosnic1@mips.com.hr>
2022-03-10 17:17:45 +0100

    added Choose an extension

--- src/extension-support/extension-worker.js
+++ src/extension-support/extension-worker.js
@@ -6,7 +6,7 @@ const dispatch = require('../dispatch/worker-dispatch');
 const TargetType = require('../extension-support/target-type');
 
 class ExtensionWorker {
-    constructor () {
+    constructor() {
         this.nextExtensionId = 0;
 
         this.initialRegistrations = [];
@@ -15,6 +15,7 @@ class ExtensionWorker {
             dispatch.call('extensions', 'allocateWorker').then(x => {
                 const [id, extension] = x;
                 this.workerId = id;
+                this.extensionURL = extension;
 
                 try {
                     importScripts(extension);
@@ -32,12 +33,12 @@ class ExtensionWorker {
         this.extensions = [];
     }
 
-    register (extensionObject) {
+    register(extensionObject) {
         const extensionId = this.nextExtensionId++;
         this.extensions.push(extensionObject);
         const serviceName = `extension.${this.workerId}.${extensionId}`;
         const promise = dispatch.setService(serviceName, extensionObject)
-            .then(() => dispatch.call('extensions', 'registerExtensionService', serviceName));
+            .then(() => dispatch.call('extensions', 'registerExtensionService', serviceName, this.extensionURL));
         if (this.initialRegistrations) {
             this.initialRegistrations.push(promise);
         }
